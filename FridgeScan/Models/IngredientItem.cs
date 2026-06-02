6542ff854295be4189e5b871723c9906232ce585
Sergio <sergiocapozzi@outlook.com>
2026-06-02 21:55:25 +0100

    feat: add IngredientItem view model for strikethrough checklist

---
+++ FridgeScan/Models/IngredientItem.cs
@@ -0,0 +1,29 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FridgeScan.Models;
+
+public partial class IngredientItem : ObservableObject
+{
+    public IngredientItem(string name)
+    {
+        Name = name;
+    }
+
+    [ObservableProperty]
+    private string name;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(TextColor))]
+    [NotifyPropertyChangedFor(nameof(Opacity))]
+    [NotifyPropertyChangedFor(nameof(TextDecorations))]
+    private bool isChecked;
+
+    public TextDecorations TextDecorations
+        => IsChecked ? TextDecorations.Strikethrough : TextDecorations.None;
+
+    public double Opacity
+        => IsChecked ? 0.5 : 1.0;
+
+    public Color TextColor
+        => IsChecked ? Color.FromArgb("#8888AA") : Color.FromArgb("#CCCCDD");
+}
